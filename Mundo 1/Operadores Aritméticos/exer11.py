aefeec461062bd9e57a3672b62ca97fbbacb74fc
João SF <73809612+g7jpedro@users.noreply.github.com>
2020-11-05 17:32:13 -0300

    Operadores Atméticos

---
+++ Mundo 1/Operadores Aritméticos/exer11.py
@@ -0,0 +1,4 @@
+a = float(input('Digite  altura:'))
+l = float(input('Digite a largura:'))
+tin = (a*l)/2
+print(f'Serão necessários {tin} litros de tinta para pintar.')
---
+++ Mundo 1/Operadores Aritméticos/exer7.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite sua primeira nota:'))
+n2 = float(input('Digite sua segunda nota:'))
+med = (n1+n2)/2
+print(f'A média é = {med}')
