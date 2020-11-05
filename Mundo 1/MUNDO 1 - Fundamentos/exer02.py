9ad8b77729695640ad156fa6d44400087d1d0842
João SF <73809612+g7jpedro@users.noreply.github.com>
2020-11-05 16:38:38 -0300

    Update exer02.py

--- Mundo 1/MUNDO 1 - Fundamentos/exer02.py
+++ Mundo 1/MUNDO 1 - Fundamentos/exer02.py
@@ -1,3 +1,4 @@
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
 n = input('Digite algo: ')
 print(f'É alfabético ?\n{n.isalpha()}')
 print(f'É númerico ?\n{n.isnumeric()}')
